2c5280377b52f3644e89d03f8da6073074c240c1
Nikitas Touloumtzis <p15toul@ionio.gr>
2018-11-05 00:08:55 +0200

    Update script_1.js

--- assets/scripts/script_1.js
+++ assets/scripts/script_1.js
@@ -152,7 +152,9 @@ function donutChart() {
                         .style('fill', colour(data.data[category])) // colour based on category mouse is over
                         .style('fill-opacity', 0.35);
 
-
+                    var msg = new responsiveVoice(toolTipHTML(data));
+                         responsiveVoice.cancel(msg);
+                         responsiveVoice.speak(msg);
                      
 
                 });
