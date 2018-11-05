306688252a57d881163de7f9c012a7e7e9d7861c
Nikitas Touloumtzis <p15toul@ionio.gr>
2018-11-05 02:37:51 +0200

    Update script_1.js

--- assets/scripts/script_1.js
+++ assets/scripts/script_1.js
@@ -152,7 +152,7 @@ function donutChart() {
                         .style('fill', colour(data.data[category])) // colour based on category mouse is over
                         .style('fill-opacity', 0.35);
                      
-                  responsiveVoice.speak(toolTipSpeaking(data));
+                  responsiveVoice.speak(toolTipHTML(data));
                 });
 
                 // remove the tooltip when mouse leaves the slice/label
