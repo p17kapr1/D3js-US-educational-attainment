6e0b871f230282010769dcf3a4c15d7fe880f83b
Nikitas Touloumtzis <p15toul@ionio.gr>
2018-11-05 17:45:41 +0200

    Update script_2.js

--- assets/scripts/script_2.js
+++ assets/scripts/script_2.js
@@ -43,7 +43,7 @@ var grandparent = svg.append("g")
         .attr("y", -margin.top)
         .attr("width", width)
         .attr("height", margin.top)
-        .attr("fill", '#00802B');
+        .attr("fill", '#009933');
 
     grandparent.append("text")
         .attr("x", 10)
@@ -87,7 +87,7 @@ d3.json("assets/data/us.json", function(data) {
             .datum(d.parent)
             .select("rect")
             .attr("fill", function () {
-                return '#f7af72'
+                return '#009933'
             });
         var g1 = svg.insert("g", ".grandparent")
             .datum(d)
