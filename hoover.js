9fe1f77dfd2b185bbfa86bf63d7667aba3cc1d57
p17kapr1 <44292629+p17kapr1@users.noreply.github.com>
2018-11-07 20:29:19 +0200

    Update hoover.js

--- hoover.js
+++ hoover.js
@@ -2,7 +2,7 @@ function mover () {
  if (navigator.appName == "Microsoft Internet Explorer" && (navigator.appVersion.indexOf ("MSIE 7")!=-1) || (navigator.appVersion.indexOf ("MSIE 8")!=-1 )) {
   if (document.all)
   {
-   document.all.sound.src = "windows98q.mp3";
+   document.all.sound.src = "oof.mp3";
   }
  }
  
