831954a10931db9c4bd529f505014982e2d63d33
p17kapr1 <44292629+p17kapr1@users.noreply.github.com>
2018-11-07 19:15:21 +0200

    Create hoover.js

---
+++ hoover.js
@@ -0,0 +1,15 @@
+function mOver() {
+if (navigator.appName == "Microsoft Internet Explorer" && (navigator.appVersion.indexOf("MSIE 7")!=-1) || (navigator.appVersion.indexOf("MSIE 8")!=-1)) {
+if (document.all)
+ {
+  document.all.sound.src = "hoverSound.mp3";
+ }
+}
+
+else {
+{
+var audio = document.getElementsByTagName("audio")[0];
+audio.play();
+}
+}
+}
